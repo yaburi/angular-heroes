cb9089e2512ac2e35b2964ac96d4a38a1b166d48
yaburi <nathan.au@hotmail.com>
2018-02-14 20:49:15 +1100

    6. Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,28 @@ import { FormsModule } from '@angular/forms'; // NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        HeroDetailComponent,
+        MessagesComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule
+    ],
+    providers: [
+        HeroService,
+        MessageService,    
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }

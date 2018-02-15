14ed1e7b612b34a2b9f4ca285e271ec281d437c2
yaburi <nathan.au@hotmail.com>
2018-02-15 17:09:52 +1100

    started 8. http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,15 +6,33 @@ import { of } from 'rxjs/observable/of';
 
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
 @Injectable()
 export class HeroService {
 
-    constructor(private messageService: MessageService) { }
+    private heroesUrl = 'api/heroes';
 
+    constructor(
+        private http: HttpClient,
+        private messageService: MessageService) { }
+
+    private log(message: string) {
+        this.messageService.add('HeroService: ' + message);
+    }
+
+/* OLD getHeroes()
     getHeroes(): Observable<Hero[]> {
         // TODO: send the message _after_ fetching the heroes
         this.messageService.add('HeroService: fetched heroes');
         return of(HEROES);
+    }
+*/
+
+// NEW getHeroes()
+    getHeroes(): Observable<Hero[]> {
+        return this.http.get<Hero[]>(this.heroesUrl);
     }
 
     getHero(id: number): Observable<Hero> {
